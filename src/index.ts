4a0815fe8be25db91bd205af7a1cae3bee8f9443
Jide Adedeji <babajide@union54.com>
2022-09-20 13:10:16 +0100

    update swagger

--- src/index.ts
+++ src/index.ts
@@ -33,6 +33,7 @@ export const socket = createSocket({ io, service })
 const PORT = process.env.PORT || 4000
 
 server.listen(PORT, () => {
+  console.log('listening on port ' + PORT)
   logger.info('listening on port ' + PORT)
 })
 
