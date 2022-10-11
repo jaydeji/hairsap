fbb3e3607616fd8f626b1e9ae4a6bade2277c35c
Jide Adedeji <babajide@union54.com>
2022-10-11 21:37:37 +0100

    fix admin get users

--- src/schemas/request/getAllUsers.ts
+++ src/schemas/request/getAllUsers.ts
@@ -1,9 +1,11 @@
 import { z } from 'zod'
+import { PageReqSchema } from './Page'
 
 export const GetAllUsersReqSchema = z
   .object({
     name: z.string().optional(),
   })
+  .merge(PageReqSchema)
   .strict()
 
 export type GetAllUsersReq = z.infer<typeof GetAllUsersReqSchema>
