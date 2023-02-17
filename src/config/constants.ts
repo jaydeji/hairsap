ec0fed522e0d4672091d14d85536c494431d027f
Jide Adedeji <babajide@union54.com>
2023-02-17 07:31:55 +0100

    change redeem cash threshold

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -58,7 +58,7 @@ export const PERIODIC_CASH_AMOUNTS = {
   // WEEKLY_EARNING: 125_000 * 100, //affected by deductions and bonus
   DAILY_TASK_TARGET: 30_000 * 100,
   MONTHLY_TASK_TARGET: 1_400_000 * 100,
-  DAILY_REDEEM_THRESHOLD: 50_000 * 100,
+  DAILY_REDEEM_THRESHOLD: 30_000 * 100,
   WEEKLY_TASK_TARGET: 150_000 * 100, //affected by deductions and bonus and weekly booking number is above 25
   WEEKLY_BONUS: 25_000 * 100,
   WEEKLY_BONUS_QUOTA: 420_000 * 100,
