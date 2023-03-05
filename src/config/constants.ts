fb366d8503a4e642c29c3f19fcc49e3d29dfd31f
Jide Adedeji <babajide@union54.com>
2023-03-05 16:27:30 +0100

    changed task target

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -56,10 +56,10 @@ export const STORAGE_ENDPOINT_CDN =
 
 export const PERIODIC_CASH_AMOUNTS = {
   // WEEKLY_EARNING: 125_000 * 100, //affected by deductions and bonus
-  DAILY_TASK_TARGET: 30_000 * 100,
+  DAILY_TASK_TARGET: 20_000 * 100,
   MONTHLY_TASK_TARGET: 1_400_000 * 100,
   DAILY_REDEEM_THRESHOLD: 30_000 * 100,
-  WEEKLY_TASK_TARGET: 150_000 * 100, //affected by deductions and bonus and weekly booking number is above 25
+  WEEKLY_TASK_TARGET: 120_000 * 100, //affected by deductions and bonus and weekly booking number is above 25
   WEEKLY_BONUS: 25_000 * 100,
   WEEKLY_BONUS_QUOTA: 420_000 * 100,
   PRO_EARNING_PERCENT: 0.7,
