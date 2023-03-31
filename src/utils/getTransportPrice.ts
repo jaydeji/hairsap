207aa2480f4108c371af5c44140a42f18a5040f6
Jide Adedeji <babajide@union54.com>
2023-03-31 20:10:06 +0100

    reduce transport fare

--- src/utils/getTransportPrice.ts
+++ src/utils/getTransportPrice.ts
@@ -1,5 +1,5 @@
 const baseFare = 300
-const pricePerKm = 175
+const pricePerKm = 100
 
 export const getTransportPrice = (distaceInMetres: number): number => {
   const amount = (baseFare + (distaceInMetres / 1000) * pricePerKm) * 100
